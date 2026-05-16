dfeaef9f3e85ced9a114935d5629534466a5ba5c
Sree Vardhan Reddy <sreevardhan1729@gmail.com>
2026-05-16 18:13:10 +0530

    added handler after creating user

---
+++ api/user/complete-onboarding.ts
@@ -0,0 +1,16 @@
+import { createHandler } from '../../src/utils/api-handler';
+import { authMiddleware } from '../../src/middleware/auth.middleware';
+import { userService } from '../../src/services/user.service';
+
+export default createHandler({
+  method: 'POST',
+  middleware: [authMiddleware],
+  handler: async (req, res) => {
+    const user = await userService.completeOnboarding(req.user!.sub);
+
+    res.status(200).json({
+      success: true,
+      data: { user },
+    });
+  },
+});
